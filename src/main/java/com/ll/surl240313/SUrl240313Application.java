7b3f0ff89d371daa856a25b24823c4ebb1329ad2
seoyeon <tus0926@naver.com>
2024-03-13 16:08:54 +0900

    start

---
+++ src/main/java/com/ll/surl240313/SUrl240313Application.java
@@ -0,0 +1,13 @@
+package com.ll.surl240313;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SUrl240313Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SUrl240313Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/surl240313/SUrl240313ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.surl240313;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SUrl240313ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
