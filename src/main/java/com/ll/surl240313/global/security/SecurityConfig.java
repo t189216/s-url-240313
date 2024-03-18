ec86b24dc6687feb2b1b760d9c6782b7e0c4b3db
seoyeon <tus0926@naver.com>
2024-03-19 02:53:56 +0900

    https://cdpn.io 에 대해서 CORS 허용, 주소 단축 폼 구현

--- src/main/java/com/ll/surl240313/global/security/SecurityConfig.java
+++ src/main/java/com/ll/surl240313/global/security/SecurityConfig.java
@@ -3,12 +3,14 @@ package com.ll.surl240313.global.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
     private final AuthenticationFilter authenticationFilter;
 
