1398b4df27d4166dda27b04f5f4da7c0fe99620a
seoyeon <tus0926@naver.com>
2024-03-18 21:26:54 +0900

    SecurityUser 객체

--- src/main/java/com/ll/surl240313/domain/surl/surl/controller/ApiV1SurlController.java
+++ src/main/java/com/ll/surl240313/domain/surl/surl/controller/ApiV1SurlController.java
@@ -3,11 +3,13 @@ package com.ll.surl240313.domain.surl.surl.controller;
 import com.ll.surl240313.domain.member.member.entity.Member;
 import com.ll.surl240313.domain.member.member.service.MemberService;
 import com.ll.surl240313.domain.surl.surl.service.SurlService;
+import com.ll.surl240313.global.security.SecurityUser;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -32,6 +34,7 @@ public class ApiV1SurlController {
             @Valid @RequestBody SurlCreateReqBody reqBody,
             Principal principal
     ) {
+        SecurityUser user = principal == null ? null :  (SecurityUser) ((Authentication) principal).getPrincipal();
         log.debug("principal: {}", principal);
         Member author = memberService.findById(4L).get();
         surlService.create(author, reqBody.url, reqBody.title);
